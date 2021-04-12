feccbeddffe5b1f037e1414bf1d74a2373f94e70
Bruno Bonavigo <b.bonavigo@gmail.com>
2021-04-12 08:22:36 -0300

    Adicionar arquivos de projeto.

---
+++ Multicontas/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Multicontas
+{
+    partial class Multicontas
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Multicontas));
+            this.btn_new_account = new System.Windows.Forms.Button();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_question = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_new_account
+            // 
+            this.btn_new_account.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_new_account.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_new_account.Location = new System.Drawing.Point(35, 33);
+            this.btn_new_account.Name = "btn_new_account";
+            this.btn_new_account.Size = new System.Drawing.Size(223, 28);
+            this.btn_new_account.TabIndex = 0;
+            this.btn_new_account.Text = "Nova Conta";
+            this.btn_new_account.UseVisualStyleBackColor = true;
+            this.btn_new_account.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_reset.Location = new System.Drawing.Point(35, 76);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(223, 28);
+            this.btn_reset.TabIndex = 1;
+            this.btn_reset.Text = "Resetar";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_question
+            // 
+            this.btn_question.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_question.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.btn_question.Location = new System.Drawing.Point(35, 120);
+            this.btn_question.Name = "btn_question";
+            this.btn_question.Size = new System.Drawing.Size(223, 28);
+            this.btn_question.TabIndex = 2;
+            this.btn_question.Text = "Funcionamento";
+            this.btn_question.UseVisualStyleBackColor = true;
+            this.btn_question.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Multicontas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 182);
+            this.Controls.Add(this.btn_question);
+            this.Controls.Add(this.btn_reset);
+            this.Controls.Add(this.btn_new_account);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Multicontas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Multicontas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_new_account;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_question;
+    }
+}
+
---
+++ Multicontas/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace Multicontas
+{
+    public partial class Multicontas : Form
+    {
+        public Multicontas()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = Environment.UserName;
+            string[] path = { @"C:\Users", userName, "AppData", "Local", "Sulake", "Habbo Launcher", "HabboFlash", "META-INF", "AIR", "application.xml" };
+            string fullPath = Path.Combine(path);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fullPath);
+            var elementos = doc.SelectNodes("//*");
+            for (int i = 0; i < elementos.Count; i++)
+            {
+                var elemento = elementos[i];
+                if (elemento.Name == "id")
+                {
+                    elemento.InnerText = "com.sulake.habboair" + DateTimeOffset.Now.ToUnixTimeSeconds();
+                    MessageBox.Show("Arquivo alterado com sucesso!", "Sucesso!");
+                }
+            }
+            doc.Save(fullPath);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string userName = Environment.UserName;
+            string[] path = { @"C:\Users", userName, "AppData", "Local", "Sulake", "Habbo Launcher", "HabboFlash", "META-INF", "AIR", "application.xml" };
+            string fullPath = Path.Combine(path);
+            XmlDocument doc = new XmlDocument();
+            doc.Load(fullPath);
+            var elementos = doc.SelectNodes("//*");
+            for (int i = 0; i < elementos.Count; i++)
+            {
+                var elemento = elementos[i];
+                if (elemento.Name == "id")
+                {
+                    elemento.InnerText = "com.sulake.habboair";
+                    MessageBox.Show("Resetado com sucesso!", "Sucesso!");
+                }
+            }
+            doc.Save(fullPath);
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            MessageBox.Show("Para poder abrir outra conta, clique em \"Nova Conta\", logue no site do Habbo e abra o Launcher.\nVocê pode repetir o processo quantas vezes quiser.\nAssim que terminar, clique em \"Resetar\" para colocar o id padrão novamente.", "Funcionamento");
+        }
+    }
+}
